2cb247cc99e204a48bc9a60fc46727fd8186cf0b
Mircea Antonescu <mircea.ant1997@utexas.edu>
2019-01-29 20:24:05 -0600

    First Commit

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    //Mircea is driving
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SubmitAdress(){
+
+    }
+
+    //Jason is driving
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
